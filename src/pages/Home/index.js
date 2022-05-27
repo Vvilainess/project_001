17e25bd84cdd30f08838c9f160092ece4d6cefd8
Vvilainess <phungtruonggiang2507@gmail.com>
2022-05-27 17:27:52 +0700

    .

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Header from "../../components/Layout/components/Header";
 import styles from "./Home.module.scss";
 import className from "classnames/bind";
@@ -6,13 +6,23 @@ import className from "classnames/bind";
 const cx = className.bind(styles);
 
 const Home = () => {
+    const homeSecEl = useRef(null);
+    const handleResize = () => {
+        window.onscroll = () => {
+            console.log(homeSecEl);
+        };
+    };
+    useEffect(() => {
+        handleResize();
+    }, []);
     return (
-        <div className={cx("home")}>
+        <div className={cx("home")} ref={homeSecEl}>
             <Header backgroundColor={"transparent"} input={false} />
             <div
                 style={{ backgroundColor: "rgb(224, 224, 224)" }}
                 className={cx("spacer")}
             ></div>
+            <section className={cx("content")}></section>
         </div>
     );
 };
